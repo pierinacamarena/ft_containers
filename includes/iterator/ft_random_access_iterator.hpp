5c218c9a5a98fb5d416ae24f684f4c9bb31de51d
Pierina Camarena <pierinaacam@gmail.com>
2022-12-19 17:55:40 +0100

    adding random access iterator

---
+++ includes/iterator/ft_random_access_iterator.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_random_access_iterator.hpp                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pierina <pierina@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/19 17:31:32 by pierina           #+#    #+#             */
+/*   Updated: 2022/12/19 17:44:46 by pierina          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef _FT_RANDOM_ACCESS_ITERATOR_HPP
+#define _FT_RANDOM_ACCESS_ITERATOR_HPP
+
+#include "ft_iterator_traits.hpp"
+#include "ft_iterator.hpp"
+
+namespace ft
+{
+	
+};
+
+#endif
