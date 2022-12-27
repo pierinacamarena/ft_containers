9ea52e345458dff9c7de02d8132eb1c4101178d8
pierinacamarena <pcamaren@student.42.fr>
2022-12-27 18:41:23 +0100

    main: added test for iterator and copy constructor

--- tests/main.cpp
+++ tests/main.cpp
@@ -6,11 +6,11 @@
 /*   By: pcamaren <pcamaren@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/26 17:40:18 by pcamaren          #+#    #+#             */
-/*   Updated: 2022/12/26 18:25:34 by pcamaren         ###   ########.fr       */
+/*   Updated: 2022/12/27 17:49:00 by pcamaren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/vector/ft_vector.hpp"
+#include "../includes/vector/vector.hpp"
 // #include "map.hpp"
 #include <iostream>
 #include <vector>
@@ -47,17 +47,85 @@ void	printf_v(T & v, std::string name)
 int main()
 {
 		// Create a vector containing integers
-		std::cout << COLOR_TWO << "/--test MAX_SIZE size capacity--/" << COLOR_NO << std::endl;
+	{		
+		std::cout << COLOR_TWO << "/--Testing: max_size(), size(), capacity()--/" << COLOR_NO << std::endl;
+		ft::vector<int>		e_m;
+		std::vector<int>	e_v;
+		ft::vector<int>		m(10, 42);
+		std::vector<int>	v(10, 42);
+		ft::vector<int>		m2(m);
+
+		std::cout << "Testing: empty constructor" << std::endl;
+		if (e_v.max_size() != e_m.max_size())
+			std::cout << "Max_size: ✘" << std::endl;
+		else
+			std::cout << "Max_size: ✔" << std::endl;
+
+		if (e_v.size() != e_m.size())
+			std::cout << "size: ✘" << std::endl;
+		else
+			std::cout << "size: ✔" << std::endl;
+
+		if (e_v.capacity() != e_m.capacity())
+			std::cout << "capacity: ✘" << std::endl;
+		else
+			std::cout << "capacity: ✔" << "\n\n";		
+
+		std::cout << "Testing: fill constructor" << std::endl;
+		if (v.max_size() != m.max_size())
+			std::cout << "Max_size: ✘" << std::endl;
+		else
+			std::cout << "Max_size: ✔" << std::endl;
+
+		if (v.size() != m.size())
+			std::cout << "size: ✘" << std::endl;
+		else
+			std::cout << "size: ✔" << std::endl;
+
+		if (v.capacity() != m.capacity())
+			std::cout << "capacity: ✘" << std::endl;
+		else
+			std::cout << "capacity: ✔" << "\n\n";
+
+		
+		// std::cout << "Testing: copy constructor " << std::endl;
+		// if (v.max_size() != m2.max_size())
+		// 	std::cout << "Max_size: ✘" << std::endl;
+		// else
+		// 	std::cout << "Max_size: ✔" << std::endl;
+
+		// if (v.size() != m2.size())
+		// 	std::cout << "size: ✘" << std::endl;
+		// else
+		// 	std::cout << "size: ✔" << std::endl;
+
+		// if (v.capacity() != m2.capacity())
+		// 	std::cout << "capacity: ✘" << std::endl;
+		// else
+		// 	std::cout << "capacity: ✔" << "\n\n";
+
+	}
+	{
+		std::cout << COLOR_TWO << "/--Testing: iterator--/" << COLOR_NO << std::endl;
 		ft::vector<int>		m(10, 42);
 		std::vector<int>	v(10, 42);
 
-		std::cout << "MAX_SIZE: " << v.max_size() << "\n";
-		std::cout << "max_size: " << m.max_size() << "\n";
-		std::cout << "SIZE: " << v.size() << "\n";
-		std::cout << "size: " << m.size() << "\n";
-		std::cout << "CAPACITY: " << v.capacity() << "\n";
-		std::cout << "capacity: " << m.capacity() << "\n";
-
+		printf_v(v, "std vector v");
+		printf_v(m, "ft vector v");
+		ft::vector<int>::iterator ft_it = m.begin();
+		std::vector<int>::iterator std_it = v.begin();
+		ft::vector<int>::iterator ft_end = m.end();
+		while (ft_it != ft_end)
+		{
+        	if (*ft_it != *std_it)
+  	          std::cout <<"iterator: ✘" << "\n";
+			ft_it++;
+			std_it++;
+		}
+  	    std::cout <<"iterator: ✔" << "\n";
+	}
+		
+		
 		// // Add two more integers to vector
 		// v.push_back(7);
 		// v.push_back(5);
@@ -67,7 +135,7 @@ int main()
 
 		// // Add two more integers to vector
 		// m.push_back(7);
-		// m.push_back(5);
+		// m.push_back(5);  
 		// m.push_back(16);
 		// m.push_back(8);
 		// m.push_back(13);
