dff970a7a7dc0be7110744126866e00659125879
pierinacamarena <pcamaren@student.42.fr>
2022-12-28 16:06:35 +0100

    vector_utils: add`enable_if` & `lexicographical`

--- includes/vector/ft_utils_vector.hpp
+++ includes/vector/ft_utils_vector.hpp
@@ -3,20 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   ft_utils_vector.hpp                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pierina <pierina@student.42.fr>            +#+  +:+       +#+        */
+/*   By: pcamaren <pcamaren@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/15 01:31:13 by pcamaren          #+#    #+#             */
-/*   Updated: 2022/12/19 10:17:18 by pierina          ###   ########.fr       */
+/*   Updated: 2022/12/28 15:46:16 by pcamaren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef _FT_UTILS_HPP_
 #define _FT_UTILS_HPP_
 
-#include "../utils/ft_enable_if.hpp"
+// #include "../utils/ft_enable_if.hpp"
 #include "../utils/ft_equal.hpp"
-#include "../utils/ft_is_integral.hpp"
-#include "../utils/ft_is_same.hpp"
+// #include "../utils/ft_is_integral.hpp"
+// #include "../utils/ft_is_same.hpp"
 #include "../utils/ft_lexicographical_compare.hpp"
 
 #endif
