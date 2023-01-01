cc8892c55bc1798b0f47d708de38e7e918d68f43
pierinacamarena <pcamaren@student.42.fr>
2023-01-01 18:17:58 +0100

    utils_vector: added includes

--- includes/vector/ft_utils_vector.hpp
+++ includes/vector/ft_utils_vector.hpp
@@ -6,17 +6,17 @@
 /*   By: pcamaren <pcamaren@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/15 01:31:13 by pcamaren          #+#    #+#             */
-/*   Updated: 2022/12/28 15:46:16 by pcamaren         ###   ########.fr       */
+/*   Updated: 2022/12/29 23:33:11 by pcamaren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef _FT_UTILS_HPP_
 #define _FT_UTILS_HPP_
 
-// #include "../utils/ft_enable_if.hpp"
+#include "../utils/ft_enable_if.hpp"
 #include "../utils/ft_equal.hpp"
-// #include "../utils/ft_is_integral.hpp"
-// #include "../utils/ft_is_same.hpp"
+#include "../utils/ft_is_integral.hpp"
+#include "../utils/ft_is_same.hpp"
 #include "../utils/ft_lexicographical_compare.hpp"
 
 #endif
