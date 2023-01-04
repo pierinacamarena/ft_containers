cf396f17efaec12508926b066c29ddfa57407ea3
pierinacamarena <pcamaren@student.42.fr>
2023-01-05 00:19:49 +0100

    rbt: implemented `decrement`

--- src/Red_Black_Tree.cpp
+++ src/Red_Black_Tree.cpp
@@ -48,7 +48,8 @@ namespace ft
 
 	rb_base_node* rb_tree_decrement(rb_base_node * x) throw()
 	{
-		if (x->color = Red && x->parent->parent )
+		if (x->color == Red && x->parent->parent == x)
+			x = x->right_child;
 		else if(x->left_child != 0)
 		{
 			x = x->left_child;
@@ -57,39 +58,36 @@ namespace ft
 		}
 		else
 		{
-			rb_base_node* y = 
+			rb_base_node* y = x->parent;
+			while (x == x->left_child)
+			{
+				x = y;
+				y = y->parent;
+			}
+			x = y;
 		}
+		return (x);
 	}
 
 	const rb_base_node* rb_tree_decrement(const rb_base_node * x) throw()
 	{
-
+				if (x->color == Red && x->parent->parent == x)
+			x = x->right_child;
+		else if(x->left_child != 0)
+		{
+			x = x->left_child;
+			while (x->right_child != 0)
+				x = x->right_child;
+		}
+		else
+		{
+			rb_base_node* y = x->parent;
+			while (x == x->left_child)
+			{
+				x = y;
+				y = y->parent;
+			}
+			x = y;
+		}
+		return (x);
 	}
-
-
-	
-         // If it is a red street shop, and the parent node of the parent is equal to himself (status 1) The right child node is a solution.
-    if (_M_node->_M_color == _S_rb_tree_red &&
-        _M_node->_M_parent->_M_parent == _M_node)
-      _M_node = _M_node->_M_right;
-         // The above situation occurs when Node is Header (ie node is end ())
-         / (Note, the HEADER's right child is MOSTRIGHT, pointing to the MAX node of the whole tree
- 
-         // If there is a left subpode (condition 2) make Y point to the left subpost, when Y has a right child node, go to the right of the right child to the end, the answer is the answer.
-    else if (_M_node->_M_left != 0) {
-      _Base_ptr __y = _M_node->_M_left;
-      while (__y->_M_right != 0)
-        __y = __y->_M_right;
-      _M_node = __y;
-    }
-         // Non-root node, there is no left child node (condition 3), find the parent node, when the current node is the left child node, always alternately, know that the current node is not left sub-node
-    else {
-      _Base_ptr __y = _M_node->_M_parent;
-      while (_M_node == __y->_M_left) {
-        _M_node = __y;
-        __y = __y->_M_parent;
-      }
-             _M_node = __y; // At this time, the parent node is the answer
-
-
-}
